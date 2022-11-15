33c0ce28a0d6e782d4b2e4f168c9705155048a38
xrander <olamideadu@gmail.com>
2022-11-15 09:25:54 +0100

    question 1 to 5 answered

--- R-analysis/Health_data_visualization/health_data.r
+++ R-analysis/Health_data_visualization/health_data.r
@@ -49,6 +49,8 @@ health_rec$Renal_failure <- as.factor(health_rec$Renal_failure)
 # Assigning values to codes
 health_rec$outcome <- ifelse(health_rec$outcome == '0', 'Alive', 'Dead')
 health_rec$gender <- ifelse(health_rec$gender == '1', 'Male', 'Female')
+health_rec$atrialfibrillation <- ifelse(health_rec$atrialfibrillation == '0', 'Having', 'Not having')
+health_rec$depression <- ifelse(health_rec$depression == 0, 'Having','Not having')
 
 # QUESTIONS
 
@@ -63,7 +65,7 @@ barplot(sort(table(health_rec$age), decreasing = T),
         ylab = 'Number of patients',
         xlab = 'Age of Patients')
 
-## ANSWER: Patients that are 89 years of age are the highest.
+  ## ANSWER: Patients that are 89 years of age are the highest.
 
 # - Question 1b: Which age group is the most in the hospital?
 breaks <- seq(from = 1, to = max(health_rec$age), by = 10) #this starts 
@@ -76,29 +78,92 @@ age_group <- cbind(data.frame(age_interval))
 
 age_groups <- sort(table(age_interval), decreasing = T) # generating frequency table
 
-
 barchart(age_groups,
          horizontal = FALSE,
          xlab = 'Age_interval',
          ylab = 'Frequency',
          ylim = c(0, 450),
          col = 'red')
-## Patients between the age 81 to 91 are the highest in the hospital with age 89 been the highest
+  ## Patients between the age 81 to 91 are the highest in the hospital with age 89 been the highest
 
 
-#which age group of patients dies more in the hospital?
-health_rec[ ,c("outcome", "age")]
+# Question 2: which age group of patients dies more in the hospital?
+
 dead_patients <- health_rec[outcome == "Dead",c("outcome","age","gender") ]
 
-barchart(age~gender, data = dead_patients,
-         group = age,
-         name)
+View(sort(table(dead_patients$age), decreasing = T)) #frequency table for dead patients according to age
+
+  ## Patients at age 89 are the highest to die, with 23 patients dying 
 
 
-sort(table(health_rec$outcome), decreasing = T)
-         
+---------------
+# Question 3: What is the most prevalent gender in the hospital
+gender <- as.data.frame(sort(table(health_rec$gender), decreasing = T)) # This gives a frequency of the genders
+  ## Female gender is more prevalent with 618 patients
 
-health_rec[,4]
+colnames(gender) <- c('gender','num') #changing column names
 
-health_rec %>% outcome %>% summarize()
+# Question 3b: what is the death rate of both gender
+gender_dead_count <- as.data.frame(sort(table(dead_patients$gender))) #Frequency for dead patients according to gender
+colnames(gender_dead_count) <- c('gender', 'count')
 
+gender[3] <- gender_dead_count$count
+
+names(gender)[3] <- 'dead' # this is similar to using colnames
+
+gender$rate <- gender$dead/gender$num * 100 #rate of death per hundred
+
+  ## the male gender is having more dead rate than female
+
+barplot(gender$rate,
+        names.arg = gender$gender,
+        xlab = substitute(paste(bold('gender'))),
+        ylab = substitute(paste(bold('death rate per hundred'))),
+        main = 'Death rate of genders',
+        col = c(5,7))
+
+--------------
+
+# Question 4: Gender group with the highest number of death
+View(sort(table((dead_patients$gender)), decreasing = T))   #the gender with the 
+  ## highest death is male with 80 individuals dead
+
+--------------
+
+# Question 5: how many patients died in the hospital with atrialfibrillation?
+dead_patients_atrial <- as.data.frame(health_rec %>% select(outcome, atrialfibrillation) %>% 
+  filter(outcome == 'Dead') %>% group_by(atrialfibrillation)%>%
+  summarize(length = length(atrialfibrillation)))
+  ## 67 patients died having atrialfibrillation
+
+# Question 5b: number of patients that died between the gender having atrialfibrillation
+dead_patients_atrial_gender <- as.data.frame(health_rec %>% select(outcome, gender, atrialfibrillation) %>% 
+    filter(outcome == 'Dead', atrialfibrillation=='Having') %>%
+    group_by(gender) %>%
+    summarize(length = length(gender)))
+  ## 39 female and 28 male died having atrialfibrillation
+gender <- merge(gender, dead_patients_atrial_gender, by = 'gender', all = TRUE) # merging the result to gender table
+names(gender)[5] <- 'died_having_atrialfibrilation'
+
+# Question 6: how many patients in the hospital have depression?
+View(sort(table(health_rec$depression)))
+
+# Question 6b: show the number of patients having depression that are dead according to gender.
+# Question 6c: Show the number of dead patients having or not having depression according to the genders
+health_rec %>% select(outcome, gender, depression) %>%
+  filter(outcome == 'Dead') %>%
+  group_by(gender, depression) %>%
+  summarize(depressed = length(depression), m_f = length(gender))
+
+
+
+# Question 7:  Is there a correlation between depression and aging?
+# Question 8: what is the rate of non-survived patients with hypertension?
+# Question 9: Rate of gender with hypertension"
+# How many patients with renal failure are alive in the hospital?
+# how many patients in the hospital with Hperlipemia are dead?
+# how many patients in the hospital with Anemia are dead?
+# What is the proportion of survival and non-survival between diabetic and non-diabetic patients
+# What is the proportion of survival and non-survival between depressed and non-depressed patients
+  # (a) What is the proportion of depressed and non-depressed that survived
+  # (b) What is the proportion of depressed and non-depressed that did not survive
